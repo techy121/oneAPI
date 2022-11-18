dc8acdc0cbace539d41baa2ba6a9e654a1b32f5e
techy121 <65383017+techy121@users.noreply.github.com>
2022-11-18 11:00:31 +0530

    Add files via upload

---
+++ 4SwapNumbersUsingTemporaryVariable.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<time.h>
+
+int main() {
+  double first, second, temp;
+  double time_spent = 0.0;
+ 
+	clock_t begin = clock();
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
+
+  // value of first is assigned to temp
+  temp = first;
+
+  // value of second is assigned to first
+  first = second;
+
+  // value of temp (initial value of first) is assigned to second
+  second = temp;
+
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
+  clock_t end = clock();
+	time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+ 
+   	printf("The elapsed time is %f seconds", time_spent);
+  return 0;
+}
+
